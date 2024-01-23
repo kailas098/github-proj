556c661c219b257ab6a03ab5d64d844b7a79089c
Kailas <kailasninja@gmail.com>
2024-01-23 10:54:04 +0530

    palindrome feature added to test.java

--- test.java
+++ test.java
@@ -1,13 +1,24 @@
-class joy
-{
-	public static void main(String args[])
-	{
-		int q = 33;
-		System.out.println(q); 
-		for(int i = 0;i<5;i++)
+class joy {
+	public static void main(String args[]) {
+		// int q = 33;
+		// System.out.println(q);
+		// for (int i = 0; i < 5; i++) {
+		// 	System.out.print(i);
+		// }
+		// System.out.println("hello");
+
+		System.out.println(palindrome("kailas"));
+	}
+
+	public static String palindrome(String s) {
+		StringBuffer sb =  new StringBuffer();
+		int len = s.length();
+
+		for(int i = 0;i<len;i++)
 		{
-			System.out.print(i);
+			sb.append(s.charAt(len-i-1));
 		}
-		System.out.println("hello");
+
+		return sb.toString();
 	}
 }
