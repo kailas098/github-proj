0c9cfca79a149e8c797d4c7c3670e04e26b99091
Kailas <kailasninja@gmail.com>
2024-01-23 11:43:33 +0530

    Uncommented code in test.java

--- test.java
+++ test.java
@@ -1,11 +1,11 @@
 class joy {
 	public static void main(String args[]) {
-		// int q = 33;
-		// System.out.println(q);
-		// for (int i = 0; i < 5; i++) {
-		// 	System.out.print(i);
-		// }
-		// System.out.println("hello");
+		int q = 33;
+		System.out.println(q);
+		for (int i = 0; i < 5; i++) {
+			System.out.print(i);
+		}
+		System.out.println("hello");
 
 		System.out.println(palindrome("kailas"));
 	}
